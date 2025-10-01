c9b7f8ca592746f47519ce55d1798e584aba1836
MIguel Ruiz <miguel.almacenamiento2010@gmail.com>
2025-10-01 17:49:09 -0500

    1-10-25
    version actualizada

---
+++ ParejasApp 1-10-25/app/src/main/java/com/example/parejasapp/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.parejasapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    //<editor-fold desc="IMAGENES">
+    lateinit var iv_11:ImageView
+    lateinit var iv_12:ImageView
+    lateinit var iv_13:ImageView
+    lateinit var iv_14:ImageView
+
+    lateinit var iv_21:ImageView
+    lateinit var iv_22:ImageView
+    lateinit var iv_23:ImageView
+    lateinit var iv_24:ImageView
+
+    lateinit var iv_31:ImageView
+    lateinit var iv_32:ImageView
+    lateinit var iv_33:ImageView
+    lateinit var iv_34:ImageView
+
+
+    //</editor-fold>
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
